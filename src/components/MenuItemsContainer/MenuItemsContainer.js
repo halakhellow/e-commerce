7645f9b2ac39d67c8a6d09a481e0378100c50856
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-09 12:20:49 +0300

    initial state for the items & render the MenuItem component

--- src/components/MenuItemsContainer/MenuItemsContainer.js
+++ src/components/MenuItemsContainer/MenuItemsContainer.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import MenuItem from "../MenuItem/MenuItem";
 
 class MenuItemsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        { title: "Hats", id: 0 },
+        { title: "Jackets", id: 1 },
+        { title: "Shoes", id: 2 },
+        { title: "Womens", id: 3 },
+        { title: "Mens", id: 4 },
+      ],
+    };
+  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {this.state.items.map((item) => (
+          <MenuItem key={item.id} title={item.title} />
+        ))}
+      </div>
+    );
   }
 }
 
