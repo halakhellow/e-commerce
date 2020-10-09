a5684454d9b2a1a96274a854514ecc99039e6fac
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-09 12:06:29 +0300

    render the HomePage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div>
+      <HomePage />
     </div>
   );
 }
