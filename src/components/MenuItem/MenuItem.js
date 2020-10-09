c5e30fe8fef45a8f6f0759f979650ff3ae6eb899
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-09 12:20:04 +0300

    add elements to be rendered

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 class MenuItem extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div>
+          <h1>{this.props.title}</h1>
+          <p>Shop Now!</p>
+        </div>
+      </div>
+    );
   }
 }
 
