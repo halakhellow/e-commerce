4615c6f1116e39a4f252ceb95ed8f641696e2a47
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-10 23:25:13 +0300

    Updates

---
+++ precache-manifest.318ec586eb278f3ef916f75ab879ed5a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e7834c183e2571580aa81daf3328974",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "c398945463b30fe8524d",
+    "url": "/e-commerce/static/css/main.33e497fa.chunk.css"
+  },
+  {
+    "revision": "209d08bcb0bfc6da58e0",
+    "url": "/e-commerce/static/js/2.66b6b52d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/e-commerce/static/js/2.66b6b52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c398945463b30fe8524d",
+    "url": "/e-commerce/static/js/main.9f938af3.chunk.js"
+  },
+  {
+    "revision": "f0bd369c1ff9feedfaf5",
+    "url": "/e-commerce/static/js/runtime-main.96a20351.js"
+  },
+  {
+    "revision": "3c278c783c7565f8dd87fa641138d049",
+    "url": "/e-commerce/static/media/hats.3c278c78.jpg"
+  },
+  {
+    "revision": "2a3963ae4f3f53d250402cdc624528cc",
+    "url": "/e-commerce/static/media/jackets.2a3963ae.jpg"
+  },
+  {
+    "revision": "78e4c1bbece466e559ec26b93f10f633",
+    "url": "/e-commerce/static/media/mens.78e4c1bb.webp"
+  },
+  {
+    "revision": "d4606edd59d77edc45a4f1ff9bfc5781",
+    "url": "/e-commerce/static/media/shoes.d4606edd.jpg"
+  },
+  {
+    "revision": "30b5a722df34743b29ed06dfefc91f9c",
+    "url": "/e-commerce/static/media/womens.30b5a722.jpeg"
+  }
+]);
