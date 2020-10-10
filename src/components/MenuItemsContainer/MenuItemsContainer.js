19df981954c1e73835e9a1e59c37e35227fe2a43
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-10 23:14:49 +0300

    add images as src to state & render the items in carousel & import css file

--- src/components/MenuItemsContainer/MenuItemsContainer.js
+++ src/components/MenuItemsContainer/MenuItemsContainer.js
@@ -1,25 +1,73 @@
 import React, { Component } from "react";
 import MenuItem from "../MenuItem/MenuItem";
+import hats from "../../images/hats.jpg";
+import jackets from "../../images/jackets.jpg";
+import shoes from "../../images/shoes.jpg";
+import mens from "../../images/mens.webp";
+import womens from "../../images/womens.jpeg";
+import "./MenuItemsContainer.css";
 
 class MenuItemsContainer extends Component {
   constructor() {
     super();
     this.state = {
       items: [
-        { title: "Hats", id: 0 },
-        { title: "Jackets", id: 1 },
-        { title: "Shoes", id: 2 },
-        { title: "Womens", id: 3 },
-        { title: "Mens", id: 4 },
+        { title: "HATS", src: hats, id: 0 },
+        { title: "JACKETS", src: jackets, id: 1 },
+        { title: "SHOES", src: shoes, id: 2 },
+        { title: "WOMENS", src: womens, id: 3 },
+        { title: "MENS", src: mens, id: 4 },
       ],
     };
   }
   render() {
     return (
-      <div>
-        {this.state.items.map((item) => (
-          <MenuItem key={item.id} title={item.title} />
-        ))}
+      <div
+        id="carousel"
+        className="carousel slide carousel-fade"
+        data-ride="carousel"
+      >
+        <ol className="carousel-indicators">
+          <li data-target="#carousel" data-slide-to="0" className="active"></li>
+          <li data-target="#carousel" data-slide-to="1"></li>
+          <li data-target="#carousel" data-slide-to="2"></li>
+          <li data-target="#carousel" data-slide-to="3"></li>
+          <li data-target="#carousel" data-slide-to="4"></li>
+        </ol>
+        <div className="carousel-inner">
+          {this.state.items.map((item, index) => (
+            <MenuItem
+              key={item.id}
+              title={item.title}
+              src={item.src}
+              index={index}
+            />
+          ))}
+          <a
+            className="carousel-control-prev"
+            href="#carousel"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carousel"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
       </div>
     );
   }
