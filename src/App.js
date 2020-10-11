a848780fe51d1a93ab63aba5dbc6b7513695c397
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-11 22:21:45 +0300

    add navbar & initial Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
+import ItemsNavbar from "./components/ItemsNavbar/ItemsNavbar";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <ItemsNavbar />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" render={() => <h1>Hats</h1>} />
+      <Route exact path="/jackets" render={() => <h1>Jackets</h1>} />
+      <Route exact path="/shoes" render={() => <h1>Shoes</h1>} />
+      <Route exact path="/womens" render={() => <h1>Womens</h1>}/>
+      <Route exact path="/mens" render={() => <h1>Mens</h1>} />
     </div>
   );
 }
