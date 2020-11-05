936ec42a95eb6ab1004c088b9b69d2abf7f39bd3
Hala Alkhellow <halakhellow1996@gmail.com>
2020-11-05 22:28:43 +0200

    add the Item properties' elements

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./CollectionItem.css";
 
 class CollectionItem extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="CollectionItem">
+        <div style={{ backgroundImage: `url(${this.props.imageUrl})` }}></div>
+        <div className="CollectionItem-footer">
+          <span>{this.props.name}</span>
+          <span>${this.props.price}</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CollectionItem;
