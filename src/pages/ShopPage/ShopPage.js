dbd7ca0a84839ec94722823b5fcd7d4418b87fc1
Hala Alkhellow <halakhellow1996@gmail.com>
2020-11-05 17:25:44 +0200

    render the CollectionPreview component

--- src/pages/ShopPage/ShopPage.js
+++ src/pages/ShopPage/ShopPage.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import ShopData from "./ShopData"
+import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
 
 class ShopPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collections : ShopData
+        }
+    }
     render() {
         return (
             <div>
+                {this.state.collections.map((collection => 
+                  <CollectionPreview key={collection.id} collection = {collection}/>
+                ))}
             </div>
         )
     }
