24788cf8a216fa4b7f143d509fa1f8805e1c622f
Hala Alkhellow <halakhellow1996@gmail.com>
2020-11-05 16:44:18 +0200

    add CollectionPreview Component

---
+++ src/components/CollectionPreview/CollectionPreview.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./CollectionPreview.css"
+
+class CollectionPreview extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CollectionPreview;
