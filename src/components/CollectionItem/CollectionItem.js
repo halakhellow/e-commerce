62d3411f75130fc18a61ba8a2914febdbff436fb
Hala Alkhellow <halakhellow1996@gmail.com>
2020-11-05 21:21:05 +0200

    add CollectionItem component

---
+++ src/components/CollectionItem/CollectionItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./CollectionItem.css";
+
+class CollectionItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CollectionItem;
