c0a318a1ad49f0d2c8340e42a1d82f55f0efbe78
Hala Alkhellow <halakhellow1996@gmail.com>
2020-11-05 16:43:37 +0200

    add ShopPage initial component

---
+++ src/pages/ShopPage/ShopPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ShopPage extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
