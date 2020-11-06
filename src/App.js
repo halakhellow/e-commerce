58130e322604aa4a9e50f1a3d5e7862ce033e038
Hala Alkhellow <halakhellow1996@gmail.com>
2020-11-07 00:57:04 +0200

    add Route for the Register page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import ItemsNavbar from "./components/ItemsNavbar/ItemsNavbar";
 import { Route } from "react-router-dom";
 import ShopPage from "./pages/ShopPage/ShopPage";
 import Header from "./components/Header/Header";
-import SignIn from "./pages/SignIn & Register/SignIn";
+import SignIn from "./pages/SignIn/SignIn";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Route exact path={["/e-commerce", "/"]} component={HomePage} />
       <Route exact path="/shop" component={ShopPage} />
       <Route exact path="/sign-in" component={SignIn} />
+      <Route exact path="/register" component={Register} />
       <Route exact path="/hats" render={() => <h1>Hats</h1>} />
       <Route exact path="/jackets" render={() => <h1>Jackets</h1>} />
       <Route exact path="/shoes" render={() => <h1>Shoes</h1>} />
