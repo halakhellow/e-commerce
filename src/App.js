29d4a4ebfebc0af02e66fdb1afe0a70f64cc34eb
Hala Alkhellow <halakhellow1996@gmail.com>
2020-11-25 23:10:12 +0200

    keep track of user authentication state when changing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import ItemsNavbar from "./components/ItemsNavbar/ItemsNavbar";
@@ -7,23 +7,43 @@ import ShopPage from "./pages/ShopPage/ShopPage";
 import Header from "./components/Header/Header";
 import SignIn from "./pages/SignIn/SignIn";
 import Register from "./pages/Register/Register";
+import { auth } from "./firebase/firebaseUtilities";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <ItemsNavbar />
-      <Route exact path={["/e-commerce", "/"]} component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
-      <Route exact path="/sign-in" component={SignIn} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/hats" render={() => <h1>Hats</h1>} />
-      <Route exact path="/jackets" render={() => <h1>Jackets</h1>} />
-      <Route exact path="/shoes" render={() => <h1>Shoes</h1>} />
-      <Route exact path="/womens" render={() => <h1>Womens</h1>} />
-      <Route exact path="/mens" render={() => <h1>Mens</h1>} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unSubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <ItemsNavbar />
+        <Route exact path={["/e-commerce", "/"]} component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/sign-in" component={SignIn} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/hats" render={() => <h1>Hats</h1>} />
+        <Route exact path="/jackets" render={() => <h1>Jackets</h1>} />
+        <Route exact path="/shoes" render={() => <h1>Shoes</h1>} />
+        <Route exact path="/womens" render={() => <h1>Womens</h1>} />
+      </div>
+    );
+  }
 }
 
 export default App;
