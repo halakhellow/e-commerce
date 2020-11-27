eb95cd183030566099aae1f4fd2ea2be5536cd48
Hala Alkhellow <halakhellow1996@gmail.com>
2020-11-27 17:01:28 +0200

    add sign out button if user signed in & classNames

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { auth } from "../../firebase/firebaseUtilities";
 import "./Header.css";
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <NavLink to="/">HOME</NavLink>
+        <NavLink className="option" to="/">
+          HOME
+        </NavLink>
         <div className="Header-options">
-          <NavLink activeClassName="active" to="/shop">
+          <NavLink className="option" activeClassName="active" to="/shop">
             SHOP
           </NavLink>
-          <NavLink activeClassName="active" to="/sign-in">
-            SIGN IN
-          </NavLink>
+          {this.props.currentUser ? (
+            <span className="option" onClick={() => auth.signOut()}>
+              SIGN OUT
+            </span>
+          ) : (
+            <NavLink className="option" activeClassName="active" to="/sign-in">
+              SIGN IN
+            </NavLink>
+          )}
         </div>
       </div>
     );
