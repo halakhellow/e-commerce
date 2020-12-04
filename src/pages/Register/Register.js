c198664b3310f2578d5be4ed437fc052c4ad7dc8
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-05 00:14:17 +0200

    import the required components and funtions + add FormInputs

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import FormInput from "../../components/FormInput/FormInput";
+import CustomBtn from "../../components/CustomBtn/CustomBtn";
+import { auth, createUserProfile } from "../../firebase/firebaseUtilities";
+import "./Register.css";
 
 class Register extends Component {
   constructor() {
@@ -11,7 +15,46 @@ class Register extends Component {
     };
   }
   render() {
-    return <div>Register</div>;
+    let { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div>
+        <h4>Sign up with email and password :</h4>
+        <form>
+          <FormInput
+            handleChange={this.handleChange}
+            value={displayName}
+            type="text"
+            id="user"
+            placeholder="Name"
+            required
+          ></FormInput>
+          <FormInput
+            handleChange={this.handleChange}
+            value={email}
+            type="email"
+            id="envelope"
+            placeholder="Email"
+            required
+          ></FormInput>
+          <FormInput
+            handleChange={this.handleChange}
+            value={password}
+            type="password"
+            id="lock"
+            placeholder="Password"
+            required
+          ></FormInput>
+          <FormInput
+            handleChange={this.handleChange}
+            value={confirmPassword}
+            type="password"
+            id="lock"
+            placeholder="Confirm Password"
+            required
+          ></FormInput>
+        </form>
+      </div>
+    );
   }
 }
 
