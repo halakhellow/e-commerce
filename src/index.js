22670d903cdc2c2637860973a066854cf53090b6
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-07 00:02:18 +0200

    wrap the whole app within Provider Component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
-
-    <BrowserRouter> 
+  <Provider>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-    
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
