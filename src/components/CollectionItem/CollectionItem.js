7dfa6fdd479456f8c4d9a1caa6bd23d127986894
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-08 20:58:28 +0200

    add "add to cart" button

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import CustomBtn from "../CustomBtn/CustomBtn";
 import "./CollectionItem.css";
 
 class CollectionItem extends Component {
   render() {
     return (
       <div className="CollectionItem">
-        <div style={{ backgroundImage: `url(${this.props.imageUrl})` }}></div>
+        <div
+          className="CollectionItem-img"
+          style={{ backgroundImage: `url(${this.props.imageUrl})` }}
+        ></div>
         <div className="CollectionItem-footer">
           <span>{this.props.name}</span>
           <span>${this.props.price}</span>
         </div>
+        <CustomBtn>Add to Cart</CustomBtn>
       </div>
     );
   }
