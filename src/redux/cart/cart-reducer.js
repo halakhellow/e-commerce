4e6371b3431681d52c4c647f3f776e43be8b99da
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-08 22:26:02 +0200

    add state for cartItems

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -2,12 +2,16 @@ import cartActionTypes from "./cart-action-types";
 
 let INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 let cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case cartActionTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
+
+    case cartActionTypes.ADD_ITEMS:
+      return { ...state, cartItems: [...state.cartItems, action.payload] };
 
     default:
       return state;
