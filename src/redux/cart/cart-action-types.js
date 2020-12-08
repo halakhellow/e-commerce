e6f1c6ce22b1069cc8fcd8e1cf8c877146e91cb8
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-08 20:01:55 +0200

    add cart redux

---
+++ src/redux/cart/cart-action-types.js
@@ -0,0 +1,5 @@
+let cartActionTypes = {
+  TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+};
+
+export default cartActionTypes;
---
+++ src/redux/cart/cart-actions.js
@@ -0,0 +1,7 @@
+import cartActionTypes from "./cart-action-types";
+
+let toggleCartHidden = {
+  type: cartActionTypes.TOGGLE_CART_HIDDEN,
+};
+
+export default toggleCartHidden;
