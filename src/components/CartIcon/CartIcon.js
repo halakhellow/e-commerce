ba468260f4cf526e03f33f45453c0a431b62a102
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-08 20:03:10 +0200

    connect component to the store to change the state

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ShoppingIcon from "../../images/cart.png";
+import { connect } from "react-redux";
 import "./CartIcon.css";
+import toggleCartHidden from "../../redux/cart/cart-actions";
 
-let CartIcon = () => (
-  <div className="CartIcon">
+let CartIcon = (props) => (
+  <div className="CartIcon" onClick={props.toggleCartHidden}>
     <img src={ShoppingIcon} className="ShoppingIcon" />
     <span className="ItemsNum">0</span>
   </div>
 );
 
-export default CartIcon;
+let mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
