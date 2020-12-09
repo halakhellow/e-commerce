e42dc86d915fe2f9aa4d5cdad20406c4ff949852
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-09 02:09:15 +0200

    displaying the number of items selected on the icon

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -7,12 +7,19 @@ import { toggleCartHidden } from "../../redux/cart/cart-actions";
 let CartIcon = (props) => (
   <div className="CartIcon" onClick={props.toggleCartHidden}>
     <img src={ShoppingIcon} className="ShoppingIcon" />
-    <span className="ItemsNum">0</span>
+    <span className="ItemsNum">{props.itemsNum}</span>
   </div>
 );
+
+let mapStateToProps = (state) => ({
+  itemsNum: state.cart.cartItems.reduce(
+    (itemsAccumulator, item) => itemsAccumulator + item.quantity,
+    0
+  ),
+});
 
 let mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
