375aa70cf1145691bcb94dc21178572d3ccc7586
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-10 18:58:08 +0200

    add empty message when there are no items & disable the buuton

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -1,19 +1,31 @@
 import React from "react";
 import CustomBtn from "../CustomBtn/CustomBtn";
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 import "./CartDropdown.css";
 import CartItem from "../CartItem/CartItem";
 import { selectCartItems } from "../../redux/cart/cart-selectors";
 import { createStructuredSelector } from "reselect";
+import emptyCart from "../../images/empty-cart.png";
 
-let CartDropdown = ({ cartItems }) => (
+let CartDropdown = ({ cartItems, history }) => (
   <div className="CartDropdown">
     <div className="CartItems">
-      {cartItems.map((item) => (
-        <CartItem key={item.id} item={item} />
-      ))}
+      {cartItems.length ? (
+        cartItems.map((item) => <CartItem key={item.id} item={item} />)
+      ) : (
+        <div className="empty-cart">
+          <img src={emptyCart} alt="empty-cart" />
+          <span>Cart is empty</span>
+        </div>
+      )}
     </div>
-    <CustomBtn>CHECKOUT</CustomBtn>
+    <CustomBtn
+      disabled={!cartItems.length}
+      onClick={() => history.push("/checkout")}
+    >
+      CHECKOUT
+    </CustomBtn>
   </div>
 );
 
@@ -21,4 +33,4 @@ let mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
