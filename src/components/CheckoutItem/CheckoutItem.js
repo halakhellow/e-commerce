9ac7559f80b0619d3cfe6af3e8534310251c287d
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-10 23:17:54 +0200

    remove the item when clicking on the x button

--- src/components/CheckoutItem/CheckoutItem.js
+++ src/components/CheckoutItem/CheckoutItem.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./CheckoutItem.css";
+import { connect } from "react-redux";
+import { removeItem } from "../../redux/cart/cart-actions";
 
-let CheckoutItem = ({ item: { name, imageUrl, price, quantity } }) => (
-  <div className="CheckoutItem">
-    <div className="img-container">
-      <img src={imageUrl} alt={name} />
+let CheckoutItem = ({ item, dispatch }) => {
+  let { name, imageUrl, price, quantity } = item;
+  return (
+    <div className="CheckoutItem">
+      <div className="img-container">
+        <img src={imageUrl} alt={name} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">${price}</span>
+      <div onClick={() => dispatch(removeItem(item))} className="remove-button">
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">${price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+export default connect()(CheckoutItem);
