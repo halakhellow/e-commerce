1313b0e6a7e433ad7aff66067df15165af10ae60
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-11 17:08:22 +0200

    dispatch increasing and decreasing actions

--- src/components/CheckoutItem/CheckoutItem.js
+++ src/components/CheckoutItem/CheckoutItem.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./CheckoutItem.css";
 import { connect } from "react-redux";
-import { removeItem } from "../../redux/cart/cart-actions";
+import {
+  addItem,
+  decreaseQuantity,
+  removeItem,
+} from "../../redux/cart/cart-actions";
 
 let CheckoutItem = ({ item, dispatch }) => {
   let { name, imageUrl, price, quantity } = item;
@@ -12,9 +16,15 @@ let CheckoutItem = ({ item, dispatch }) => {
       </div>
       <span className="name">{name}</span>
       <span className="quantity">
-        <span className="fas fa-minus"></span>
+        <span
+          className="fas fa-minus"
+          onClick={() => dispatch(decreaseQuantity(item))}
+        ></span>
         <span className="value">{quantity}</span>
-        <span className="fas fa-plus"></span>
+        <span
+          className="fas fa-plus"
+          onClick={() => dispatch(addItem(item))}
+        ></span>
       </span>
       <span className="price">${price}</span>
       <div onClick={() => dispatch(removeItem(item))} className="remove-button">
