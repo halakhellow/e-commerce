2f1955e7e1488efc850f34af40fbe943e4b816bb
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-11 17:07:18 +0200

    add decreaseItemQuantity function

--- src/redux/cart/cart-utilities.js
+++ src/redux/cart/cart-utilities.js
@@ -10,3 +10,14 @@ export let addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export let decreaseItemQuantity = (cartItems, cartItemToDecrease) => {
+  if (cartItemToDecrease.quantity === 1) {
+    return cartItems.filter((item) => item.id !== cartItemToDecrease.id);
+  }
+  return cartItems.map((item) =>
+    item.id === cartItemToDecrease.id
+      ? { ...item, quantity: item.quantity - 1 }
+      : item
+  );
+};
