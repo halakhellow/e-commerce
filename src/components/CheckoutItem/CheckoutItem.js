921d907cbce7b120d6551a760fadfdbc821a0d26
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-11 16:14:39 +0200

    add minus and plus signs to control the quantity & their initial stlyes

--- src/components/CheckoutItem/CheckoutItem.js
+++ src/components/CheckoutItem/CheckoutItem.js
@@ -11,7 +11,11 @@ let CheckoutItem = ({ item, dispatch }) => {
         <img src={imageUrl} alt={name} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <span className="fas fa-minus"></span>
+        <span className="value">{quantity}</span>
+        <span className="fas fa-plus"></span>
+      </span>
       <span className="price">${price}</span>
       <div onClick={() => dispatch(removeItem(item))} className="remove-button">
         &#10005;
