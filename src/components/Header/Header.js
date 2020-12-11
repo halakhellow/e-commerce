ca268932ab95563349c25ed16f442d811737c5b1
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-12 00:27:12 +0200

    add register option & re-arrange options

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,21 +13,34 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <NavLink className="option" to="/">
-          HOME
+        <NavLink className="option" activeClassName="active" to="/shop">
+          SHOP
         </NavLink>
-        <div className="Header-options">
-          <NavLink className="option" activeClassName="active" to="/shop">
-            SHOP
-          </NavLink>
+        <NavLink className="option logo" to="/">
+          Home
+        </NavLink>
+        <div className="last-options">
           {this.props.currentUser ? (
             <span className="option" onClick={() => auth.signOut()}>
               SIGN OUT
             </span>
           ) : (
-            <NavLink className="option" activeClassName="active" to="/sign-in">
-              SIGN IN
-            </NavLink>
+            <div>
+              <NavLink
+                className="option"
+                activeClassName="active"
+                to="/sign-in"
+              >
+                SIGN IN
+              </NavLink>
+              <NavLink
+                className="option"
+                activeClassName="active"
+                to="/register"
+              >
+                Register
+              </NavLink>
+            </div>
           )}
           <CartIcon />
         </div>
