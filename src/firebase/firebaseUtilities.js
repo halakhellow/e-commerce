98ac9322e96fd7d6ddd6513dd0d7468259b86c3d
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-16 01:08:05 +0200

    import the configuration object from a separate file

--- src/firebase/firebaseUtilities.js
+++ src/firebase/firebaseUtilities.js
@@ -1,17 +1,9 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import configObject from "./firebase-config";
 
-let config = {
-  apiKey: "AIzaSyBbUs-rvfrqceopZph1IIrFyQ5PJpj1UXg",
-  authDomain: "e-commerce-db-6b400.firebaseapp.com",
-  databaseURL: "https://e-commerce-db-6b400.firebaseio.com",
-  projectId: "e-commerce-db-6b400",
-  storageBucket: "e-commerce-db-6b400.appspot.com",
-  messagingSenderId: "664831205699",
-  appId: "1:664831205699:web:eb3554e8208d5cd12a8ff6",
-  measurementId: "G-W7KV8RM684",
-};
+let config = configObject;
 
 export let createUserDocument = async (userAuth, otherData) => {
   if (!userAuth) return;
