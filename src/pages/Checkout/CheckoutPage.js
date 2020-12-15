fd5283b1f57324855fc8363c19a8905bfcb0537d
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-15 21:01:24 +0200

    use react-beautiful-dnd to make the checkout items list draggable + use mapDispatchToProps argument to get updateCartItems action

--- src/pages/Checkout/CheckoutPage.js
+++ src/pages/Checkout/CheckoutPage.js
@@ -8,34 +8,79 @@ import {
   selectItemsTotalPrice,
 } from "../../redux/cart/cart-selectors";
 import CheckoutItem from "../../components/CheckoutItem/CheckoutItem";
+import { updateCartItems } from "../../redux/cart/cart-actions";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-let CheckoutPage = ({ cartItems, total }) => (
-  <div className="CheckoutPage">
-    <HrWithText content="Your Shopping Cart" />
-    <div className="CheckoutPage-header">
-      <div className="header-block">
-        <span>Product</span>
+let CheckoutPage = ({ cartItems, total, updateCartItems }) => {
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    if (
+      result.source.droppableId === result.destination.droppableId &&
+      result.source.index === result.destination.index
+    )
+      return;
+    let updatedItems = cartItems;
+    let [reorderedItem] = updatedItems.splice(result.source.index, 1);
+    updatedItems.splice(result.destination.index, 0, reorderedItem);
+
+    updateCartItems(updatedItems);
+  }
+  return (
+    <div className="CheckoutPage">
+      <HrWithText content="Your Shopping Cart" />
+      <div className="CheckoutPage-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
-      <div className="header-block">
-        <span>Quantity</span>
-      </div>
-      <div className="header-block">
-        <span>Price</span>
-      </div>
-      <div className="header-block">
-        <span>Remove</span>
-      </div>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="checkout-items">
+          {(provided) => (
+            <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {cartItems.map((item, index) => {
+                return (
+                  <Draggable
+                    key={item.id}
+                    draggableId={item.name}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <CheckoutItem
+                        item={item}
+                        innerRef={provided.innerRef}
+                        provided={provided}
+                      />
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+
+      <div className="total-price">Total Price : ${total}</div>
     </div>
-    {cartItems.map((item) => (
-      <CheckoutItem key={item.id} item={item} />
-    ))}
-    <div className="total-price">Total Price : ${total}</div>
-  </div>
-);
+  );
+};
 
 let mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
   total: selectItemsTotalPrice,
 });
 
-export default connect(mapStateToProps)(CheckoutPage);
+let mapDispatchToProps = (dispatch) => ({
+  updateCartItems: (items) => dispatch(updateCartItems(items)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CheckoutPage);
