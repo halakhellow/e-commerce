3e5995e620150e41d9675f774ad6176a3259bfa1
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-16 23:03:08 +0200

    set up persistConfig object to save the cart state in the local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import cartReducer from "./cart/cart-reducer";
 import userReducer from "./user/user-reducer";
+
+let persistConfig = {
+  key: "root",
+  storage,
+  whitelist: "cart",
+};
 
 let rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
