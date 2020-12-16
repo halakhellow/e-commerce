77b7b4727c560ea3f372b110f8bfea535a2aa1a7
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-17 00:31:09 +0200

    add the menuReducer to the root reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,6 +3,7 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import cartReducer from "./cart/cart-reducer";
+import menuReducer from "./menu/menu-reducer";
 import userReducer from "./user/user-reducer";
 
 let persistConfig = {
@@ -14,6 +15,7 @@ let persistConfig = {
 let rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  menu: menuReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
