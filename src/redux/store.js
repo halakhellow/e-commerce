f50baf6bebb821113deafbb8298b0bce59f66e96
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-16 22:58:55 +0200

    create a persisted store with persistStore function

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
 let middlewares = [logger];
 
 let store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+let persistor = persistStore(store);
+
+export { store, persistor };
