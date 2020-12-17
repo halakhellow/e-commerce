a8ea90649c29dad2abe26c9db7694cf19c18158b
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-18 00:11:10 +0200

    make the ShopPage a simple component without connecting to the store and render the CollectionsOverview component

--- src/pages/ShopPage/ShopPage.js
+++ src/pages/ShopPage/ShopPage.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
-import { selectShopCollections } from "../../redux/shop/shop-selectors";
+import CollectionOverview from "../../components/CollectionOverview/CollectionOverview";
 
-import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
-
-let ShopPage = ({ collections }) => (
+let ShopPage = () => (
   <div>
-    {collections.map((collection) => (
-      <CollectionPreview key={collection.id} collection={collection} />
-    ))}
+    <CollectionOverview />
   </div>
 );
 
-let mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
