bf58783006ef083555e5d172967523f6372b5747
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-17 23:04:34 +0200

    add the shopReducer to the rootReducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from "redux-persist/lib/storage";
 
 import cartReducer from "./cart/cart-reducer";
 import menuReducer from "./menu/menu-reducer";
+import shopReducer from "./shop/shop-reducer";
 import userReducer from "./user/user-reducer";
 
 let persistConfig = {
@@ -16,6 +17,7 @@ let rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   menu: menuReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
