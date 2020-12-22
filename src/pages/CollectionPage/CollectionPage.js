db52b4f051f7ebe1cfff0f274729d9a429810783
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-22 22:14:14 +0200

    render the collection title and items in the CollectionPage

--- src/pages/CollectionPage/CollectionPage.js
+++ src/pages/CollectionPage/CollectionPage.js
@@ -3,13 +3,25 @@ import { connect } from "react-redux";
 
 import { selectCollection } from "../../redux/shop/shop-selectors";
 
+import CollectionItem from "../../components/CollectionItem/CollectionItem";
+import CollectionsNavbar from "../../components/CollectionsNavbar/CollectionsNavbar";
+
 import "./CollectionPage.css";
 
-let CollectionPage = ({ collection }) => (
-  <div className="CollectionPage">
-    <h1>{collection.title}</h1>
-  </div>
-);
+let CollectionPage = ({ collection }) => {
+  let { title, items } = collection;
+  return (
+    <div className="CollectionPage">
+      <CollectionsNavbar />
+      <h1 className="CollectionPage-title">{title}</h1>
+      <div className="CollectionPage-items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 let mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collection)(state),
