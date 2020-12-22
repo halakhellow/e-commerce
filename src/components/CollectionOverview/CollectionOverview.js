466637a4a38295c78997105e65ea1f81dea45cf7
Hala Alkhellow <halakhellow1996@gmail.com>
2020-12-22 22:11:38 +0200

    remove the navbar links in CollectionOverview to be in a separate component & use Object.keys to access the collection data

--- src/components/CollectionOverview/CollectionOverview.js
+++ src/components/CollectionOverview/CollectionOverview.js
@@ -1,36 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { NavLink } from "react-router-dom";
 
 import { selectShopCollections } from "../../redux/shop/shop-selectors";
 
 import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
-
-import "./CollectionOverview.css";
+import CollectionsNavbar from "../CollectionsNavbar/CollectionsNavbar";
 
 let CollectionOverview = ({ collections }) => (
-  <div className="CollectionOverview">
-    <div className="Navbar">
-      <NavLink activeClassName="active" to="/shop/hats">
-        Hats
-      </NavLink>
-      <NavLink activeClassName="active" to="/shop/jackets">
-        Jackets
-      </NavLink>
-      <NavLink activeClassName="active" to="/shop/shoes">
-        Shoes
-      </NavLink>
-      <NavLink activeClassName="active" to="/shop/womens">
-        Womens
-      </NavLink>
-      <NavLink activeClassName="active" to="/shop/mens">
-        Mens
-      </NavLink>
-    </div>
+  <div>
+    <CollectionsNavbar />
     <div>
-      {collections.map((collection) => (
-        <CollectionPreview key={collection.id} collection={collection} />
+      {Object.keys(collections).map((key) => (
+        <CollectionPreview
+          key={collections[key].id}
+          collection={collections[key]}
+        />
       ))}
     </div>
   </div>
