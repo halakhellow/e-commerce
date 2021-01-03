ca75e2c1f2a9373992b9115bc2faa8ba3a8e0342
Hala Alkhellow <halakhellow1996@gmail.com>
2021-01-04 01:41:49 +0200

    convert the ShopPage into a class component and when mounting fetch the collections from the firestore and convert them into an object then update the store

--- src/pages/ShopPage/ShopPage.js
+++ src/pages/ShopPage/ShopPage.js
@@ -1,14 +1,40 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+import {
+  convertCollectionsSnapshotToMap,
+  firestore,
+} from "../../firebase/firebaseUtilities";
 
 import CollectionOverview from "../../components/CollectionOverview/CollectionOverview";
 import CollectionPage from "../CollectionPage/CollectionPage";
+import { updateCollections } from "../../redux/shop/shop-actions";
 
-let ShopPage = () => (
-  <div>
-    <Route exact path="/shop" component={CollectionOverview} />
-    <Route exact path="/shop/:collection" component={CollectionPage} />
-  </div>
-);
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
 
-export default ShopPage;
+  componentDidMount() {
+    let { updateCollections } = this.props;
+    let collectionsRef = firestore.collection("collections");
+    collectionsRef.onSnapshot((snapshot) => {
+      let collections = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collections);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Route exact path="/shop" component={CollectionOverview} />
+        <Route exact path="/shop/:collection" component={CollectionPage} />
+      </div>
+    );
+  }
+}
+
+let mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collections) => dispatch(updateCollections(collections)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
