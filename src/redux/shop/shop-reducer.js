5be9c97f242ce3695444d7c8a405ef32f1d56d1d
Hala Alkhellow <halakhellow1996@gmail.com>
2021-01-07 02:29:06 +0200

    add isFetching and errorMessage in the initial state in the shop reducer and return state depending on the actions for fetching collections from the firestore + add action types in the shop action types file

--- src/redux/shop/shop-reducer.js
+++ src/redux/shop/shop-reducer.js
@@ -2,14 +2,28 @@ import shopActionTypes from "./shop-action-types";
 
 let INITIAL_STATE = {
   collections: null,
+  isFetching: false,
+  errorMessage: "",
 };
 
 let shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case shopActionTypes.UPDATE_COLLECTIONS:
+    case shopActionTypes.FETCH_COLLECTIONS_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case shopActionTypes.FETCH_COLLECTIONS_SUCESS:
       return {
         ...state,
         collections: action.payload,
+        isFetching: false,
+      };
+    case shopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload,
       };
     default:
       return state;
