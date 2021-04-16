df2b54a3764eba5c98bff75bac8bb8d8124b45c3
Hala Alkhellow <halakhellow1996@gmail.com>
2021-04-17 00:37:50 +0300

    edit the path for the MenuItem Component

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -8,9 +8,11 @@ class MenuItem extends Component {
     return (
       <div className={`carousel-item ${activeClass}`} data-interval="3000">
         <img src={this.props.src} alt={this.props.title} />
-        <div 
-         onClick={() => this.props.history.push(`/${this.props.title.toLowerCase()}`)}
-         className="carousel-caption d-none d-md-block"
+        <div
+          onClick={() =>
+            this.props.history.push(`/shop/${this.props.title.toLowerCase()}`)
+          }
+          className="carousel-caption d-none d-md-block"
         >
           <h1>{this.props.title}</h1>
           <p>Shop Now!</p>
